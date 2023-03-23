ed47a5881199e1e9def175f0cd4cd0789fde7fb0
Lilia Vasileva <lilia.vasileva@uniberg.com>
2023-03-23 15:58:38 +0200

    Adding Item Controller

---
+++ server/controllers/itemsController.js
@@ -0,0 +1,34 @@
+const router = require('express').Router();
+
+const { isAuth } = require('../middlewares/authMiddleware');
+const itemService = require('../services/itemsService');
+const { getErrorMessage } = require('../utils/errorUtils');
+
+
+
+router.post('/create', isAuth, async (req, res) => {
+
+  const { name, price, category, description, image } = req.body;
+  const userId = req.user._id;
+
+  try {
+
+    await itemService.create(userId, {
+      name,
+      price,
+      category,
+      description,
+      image,
+    });
+    return res.status(200).json('This is temp message')
+
+  } catch (error) {
+    return res
+      .status(404)
+      .json({ error: getErrorMessage(error) });
+  }
+});
+
+
+
+module.exports = router;
