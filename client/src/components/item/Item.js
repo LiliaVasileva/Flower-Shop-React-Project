099238da023e4440c79299c957f865ba19a928ce
Lilia Vasileva <lilia.vasileva@uniberg.com>
2023-03-30 15:32:30 +0300

    Fix category on item to show correct label in bulgarian

--- client/src/components/item/Item.js
+++ client/src/components/item/Item.js
@@ -1,7 +1,12 @@
+function Item({_id, name, price, category, image}) {
+    const categoryValues = {
+        "funeral": "Погребения",
+        "wedding": "Сватба",
+        "assortment": "Асортимент",
+        "other": "Други"
+    }
 
-function Item({_id, name, price, category, image }) {
 
-    
     return (
         <section id="items">
             <article className="article-card">
@@ -9,7 +14,7 @@ function Item({_id, name, price, category, image }) {
                     <img src={image} alt="Flower-Item"/>
                 </figure>
                 <div className="article-content">
-                    <a href="" className="card-category">{category}</a>
+                    <h2 className="card-category">{categoryValues[category]}</h2>
                     <h3 className="card-title">{name}</h3>
                     <h3 className="card-title">Цена: {price} лв</h3>
                 </div>
