5d713c1a6ed775c7a26f12e8319a94434368777f
Lilia Vasileva <lilia.vasileva@uniberg.com>
2023-03-31 09:53:33 +0300

    Fix css for Contact page

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,15 +1,16 @@
 import {Link} from "react-router-dom";
 
+import styles from "./Contacts.module.css"
 import shop from "./images/The-Flower-Shop-Bruton-Jo.jpg"
 
 function Contacts() {
     return (
-        <section id="contacts" className="contacts">
-            <div className="contacts-info">
-                <div className="text">
+        <section id="contacts" className={styles.contacts}>
+            <div className={styles.contactInfo}>
+                <div className={styles.textContactInfo}>
                     <h2>Заповядайте, да ни посетите!</h2>
                     <p> Магазина се намира на ул. Тодор Влайков, точно срещу СОУ "Елин Пелин". </p>
-                    <ul className="address__info">
+                    <ul className={styles.addressInfo}>
                         <li>
                             <span>Адрес:</span> Тодор Влайков №41 гр.Варна България
                         </li>
@@ -17,14 +18,14 @@ function Contacts() {
                             <span>Телефон:</span> 052/ 60 20 49, +359 888 678 7033, +359 88 920 1431
                         </li>
                         <li><Link target="_blank" to="https://goo.gl/maps/uFWqfeUB9MeZCP4KA"
-                                  className="directions__button"> Упътване </Link></li>
+                                  className={styles.directionButton}> Упътване </Link></li>
                     </ul>
                 </div>
-                <div className="image">
+                <div className={styles.contactImage}>
                     <img src={shop} alt="Store"/>
                 </div>
             </div>
-            <div className="contacts-form">
+            <div className={styles.contactFormContainer}>
                 <h2>Изпратете ни, съобщение!</h2>
                 <p>Работим и по заявки на клиента, ако имате по специфични желания,
                     от показаните в страницата, свържете се с нас да ги обсъдим.
@@ -32,11 +33,11 @@ function Contacts() {
                 <p> Може да ни потърсите на телефоните оставени за контакт или да ни изпратите мейл.</p>
                 <p> Ще се радваме да обсъдим вашите предложения! </p>
 
-                <form className="form">
-                    <input name="fullname" className="fullname" placeholder="Име и Фамилия..."/>
-                    <input className="email" placeholder="Вашия мейл..."/>
-                    <input className="message" placeholder="Вашето съобщение..."   type="text"/>
-                    <input type="submit" className="submit" name="submit" value="Изпрати"/>
+                <form className={styles.contactForm}>
+                    <input name="fullname" className={styles.fullname} placeholder="Име и Фамилия..."/>
+                    <input className={styles.email} placeholder="Вашия мейл..."/>
+                    <input className={styles.message} placeholder="Вашето съобщение..."   type="text"/>
+                    <input type="submit" className={styles.submitContactButton} name="submit" value="Изпрати"/>
                 </form>
             </div>
 
