2ccb0b18cd413372187b3f3d90bbb9aec251591f
Lilia Vasileva <lilia.vasileva@uniberg.com>
2023-03-31 10:40:08 +0300

    Fix Header component CSS

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,16 +1,17 @@
 import React from "react";
 import {Link} from "react-router-dom";
 
+import styles from "./Header.module.css"
 import salvia from "./images/salvia.png";
 
 
 function Header() {
     return (
-        <header id="header">
-            <div className="logo">
-                <div className="logo__info">
-                    <Link to="/"><img alt="Salvia-png" className="logo__img" src={salvia}/></Link>
-                    <h3>Цветарски магазин Салвия</h3>
+        <header className={styles.header}>
+            <div className={styles.headerLogo}>
+                <div className={styles.logoInfo}>
+                    <Link to="/"><img alt="Salvia-png" className={styles.logoImg} src={salvia}/></Link>
+                    <h3 className={styles.headerTitle}>Цветарски магазин Салвия</h3>
                 </div>
             </div>
         </header>
