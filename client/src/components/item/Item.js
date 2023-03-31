02e845fed24ef00769c030472393783cfac394ae
Lilia Vasileva <lilia.vasileva@uniberg.com>
2023-03-31 11:19:24 +0300

    Fix Item CSS

--- client/src/components/item/Item.js
+++ client/src/components/item/Item.js
@@ -1,4 +1,7 @@
+import styles from "./Item.module.css"
+
 function Item({_id, name, price, category, image}) {
+
     const categoryValues = {
         "funeral": "Погребения",
         "wedding": "Сватба",
@@ -6,21 +9,17 @@ function Item({_id, name, price, category, image}) {
         "other": "Други"
     }
 
-
     return (
-        <section id="items">
-            <article className="article-card">
-                <figure className="article-image">
+            <article className={styles.articleCard}>
+                <figure className={styles.articleImage}>
                     <img src={image} alt="Flower-Item"/>
                 </figure>
-                <div className="article-content">
-                    <h2 className="card-category">{categoryValues[category]}</h2>
-                    <h3 className="card-title">{name}</h3>
-                    <h3 className="card-title">Цена: {price} лв</h3>
+                <div className={styles.articleContent}>
+                    <h2 className={styles.cardCategory}>{categoryValues[category]}</h2>
+                    <h3 className={styles.cardTitle}>{name}</h3>
+                    <h3 className={styles.cardTitle}>Цена: {price} лв</h3>
                 </div>
-            </article>
-        </section>
-    )
+            </article>)
 }
 
 export default Item;
