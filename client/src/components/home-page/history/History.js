be547683c309838e1cf6d7ea8a11da27a105bc91
Lilia Vasileva <lilia.vasileva@uniberg.com>
2023-03-31 17:54:45 +0300

    Fix design on home page

--- client/src/components/home-page/history/History.js
+++ client/src/components/home-page/history/History.js
@@ -1,24 +1,25 @@
-import ladies from "./slider-img-1.jpg"
+import styles from "./History.module.css"
+import ladies from "../images/slider-img-1.jpg"
 
 function History() {
     return (
-        <section id="history" className="section">
-            <div className="image"><img src={ladies} alt=""/></div>
-            <div className="info">
-                <h2 className="info__title">История</h2>
-                <p className="info__desc">Богат избор от цветни традиции</p>
-                <ul className="info__items" role="list">
-                    <li className="info__items__li ">
+        <section className={styles.history}>
+            <div className={styles.image}><img src={ladies} alt=""/></div>
+            <div className={styles.info}>
+                <h2 className={styles.infoTitle}>История</h2>
+                <p className={styles.infoDescription}>Богат избор от цветни традиции</p>
+                <ul className={styles.infoItems}>
+                    <li className={styles.infoItemsList}>
                         Магазина е основан през 1992 година, от Славка Димитрова.
                         Славка е експерт в озеляването, аранжирането и поддържането на цветя.
                         Печелила е 6 конкурса за аранжиране на венци, букети и жарданиери.
                     </li>
-                    <li className="info__items__li ">
+                    <li className={styles.infoItemsList}>
                         Магазина е основан през 1992 година, от Славка Димитрова.
                         Славка е експерт в озеляването, аранжирането и поддържането на цветя.
                         Печелила е 6 конкурса за аранжиране на венци, букети и жарданиери.
                     </li>
-                    <li className="info__items__li ">
+                    <li className={styles.infoItemsList}>
                         Магазина е основан през 1992 година, от Славка Димитрова.
                         Славка е експерт в озеляването, аранжирането и поддържането на цветя.
                         Печелила е 6 конкурса за аранжиране на венци, букети и жарданиери.
